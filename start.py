fd9e20cf80852680cc7da78995ce5435ff06930a
Pascal Schirmer <66561268+pascme05@users.noreply.github.com>
2024-06-22 23:26:47 +0200

    disable training start

--- start.py
+++ start.py
@@ -68,7 +68,7 @@ setupExp['warn'] = 3                                                            
 setupExp['method'] = 0                                                                                                   # 0) 1-fold with data split, 1) k-fold with cross validation, 2) transfer learning with different datasets, 3) id based
 setupExp['trainBatch'] = 0                                                                                               # 0) all no batching, 1) fixed batch size (see data batch parameter), 2) id based
 setupExp['kfold'] = 10                                                                                                   # number of folds for method 1)
-setupExp['train'] = 1                                                                                                    # 0) no training (trying to load model), 1) training new model (or retraining)
+setupExp['train'] = 0                                                                                                    # 0) no training (trying to load model), 1) training new model (or retraining)
 setupExp['test'] = 1                                                                                                     # 0) no testing, 1) testing
 
 # ------------------------------------------
